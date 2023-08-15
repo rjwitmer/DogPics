671b649700d62acdd2de5966f2fe7e4fbefa815a
Bob Witmer <bwitmer@golden.net>
2023-08-15 11:31:46 -0400

    Initial Commit

---
+++ DogPics/DogPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPicsApp.swift
+//  DogPics
+//
+//  Created by Bob Witmer on 2023-08-15.
+//
+
+import SwiftUI
+
+@main
+struct DogPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
